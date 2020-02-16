ca3571b698385e73e0ed8322de9a3b531e173e2c
Nathan Fischer <CannedCans@users.noreply.github.com>
2020-02-16 18:53:08 -0500

    Added backend stubs, added index.html

--- main-project/app/main.py
+++ main-project/app/main.py
@@ -6,10 +6,18 @@ import os
 app = Flask(__name__, static_url_path='', static_folder='static')
 CORS(app)
 
+def validateToken(username, token):
+    # TODO: Return a boolean validating the token is active and associated with username
+    return True
+
+def generateToken(username):
+    # TODO: Generate a new token for the username
+    pass
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     # Only for debugging while developing
