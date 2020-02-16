4ac5d4644b8af19da0a6ec52dfb398d7f83dbecc
Nathan Fischer <CannedCans@users.noreply.github.com>
2020-02-16 18:35:38 -0500

    Added starter code like stuff

---
+++ main-project/app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+import os
+
+# Start the app and setup the static directory for the html, css, and js files.
+app = Flask(__name__, static_url_path='', static_folder='static')
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, world!'
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
