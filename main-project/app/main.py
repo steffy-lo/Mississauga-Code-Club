a1308dc0db48326e70742a64d7cb4dc60e128403
Nathan Fischer <CannedCans@users.noreply.github.com>
2020-02-17 14:57:42 -0500

    Added logout route

--- main-project/app/main.py
+++ main-project/app/main.py
@@ -46,6 +46,11 @@ def authenticate():
 
     abort(401)
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
 if __name__ == "__main__":
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
