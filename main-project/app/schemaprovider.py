417c58c25e60d96e41d3f80eed6c51b9e20a4279
Nathan Fischer <CannedCans@users.noreply.github.com>
2020-03-07 12:22:56 -0500

    Minor documentation improvements

--- main-project/app/schemaprovider.py
+++ main-project/app/schemaprovider.py
@@ -1,7 +1,18 @@
 """
 SchemaFactory will provide schemas for database
 validation.
+
+The benefits include only having to generate the schemas once
+as well as decluttering main.py
 """
 
 class SchemaFactory:
     example = {'field' : {'type' : 'number'}}
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("This file should not be executed directly.")
