ddad55a1887b3f26d7f8da5a6e831d3ca026eff7
bilykand <andriyan.bilyk@mail.utoronto.ca>
2020-03-08 01:13:52 -0500

    Implemented Create Class (backend notwithstanding)

--- main-project/app/client/src/Actions/admin.js
+++ main-project/app/client/src/Actions/admin.js
@@ -8,9 +8,9 @@ export const checkIn = (email, purpose, hours, paid) => {
     axios.post("/api/checkin",
     JSON.stringify({ email, purpose, hours, paid}),
     {headers: {"Content-Type": "application/json"}})
-    .then(time => {
-      if (!time || !time.data) throw {status: 500, statusText: "Something went wrong"};
-      resolve(time.data.dateTime)
+    .then(res => {
+      if (!res || !res.data) throw {stat: 500, statusText: "Something went wrong"};
+      resolve(res.data.dateTime)
     })
     .catch(err => {
       if (err.status === 403 || err.status === 401) {
@@ -24,4 +24,31 @@ export const checkIn = (email, purpose, hours, paid) => {
       }
     })
   });
+}
+
+export const createClass = (title) => {
+  return new Promise((resolve, reject) => {
+    if (title === "") reject({stat: 400, msg: "Classnames should not be empty"});
+    axios.post("/api/newClass", JSON.stringify(title),
+    {headers: {"Content-Type": "application/json"}})
+    .then(res => {
+      if (!res || !res.data) throw {stat: 500, statusText: "Something went wrong"};
+      resolve(res.data.class_id);
+    })
+    .catch(err => {
+      standardReject(err, reject);
+    })
+  });
+}
+
+const standardReject = (err, reject) => {
+  if (err.status === 403 || err.status === 401) {
+    deauthorise();
+    reject({stat: 403, msg: "Your login has expired. Please, reauthenticate."})
+  } else {
+    reject({
+      stat: err.status,
+      msg: "There was an error processing your request. Please, try again later."
+    });
+  }
 }
--- main-project/app/client/src/Components/Admin/CreateClass.js
+++ main-project/app/client/src/Components/Admin/CreateClass.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import StatusModal from '../Util/StatusModal';
+import TwoOptionModal from '../Util/TwoOptionModal';
 import LoadingModal from '../Util/LoadingModal';
+
+import { createClass } from '../../Actions/admin';
 
 import "../CSS/Common.css";
 import "../CSS/Util/StatusModal.css";
@@ -35,7 +38,55 @@ class CreateClass extends React.Component {
             <div className="buttonSectionWrapper">
               <button className="adminStyle"
               onClick={e => {
-                this.onClose();
+                this.setState({toDisplay: <LoadingModal text="Creating class ..." />});
+                createClass(this.state.nameOfClass)
+                .then(class_id => {
+                  this.setState({toDisplay: null});
+                  this.setState({
+                    toDisplay:
+                      <StatusModal title="Class Creation Successful"
+                        text={
+                          <span>
+                            {`Class ${this.state.nameOfClass}`}
+                            <br />
+                            {`with id: ${class_id}`}
+                            <br />
+                            created
+                          </span>
+                          }
+                        textB="Edit"
+                        onClose={() => this.setState({toDisplay: null})}
+                      />
+                  })
+                })
+                .catch(err => {
+                  let clFunc = () => this.setState({toDisplay: null});
+                  if (err.stat === 403) {
+                    this.setState({toDisplay: null});
+                    this.setState({
+                      toDisplay:
+                        <LoadingModal text={
+                            <span>
+                              Your login has expired
+                              <br />
+                              Please reauthenticate
+                              <br />
+                              Singing you out ...
+                            </span>
+                        }/>
+                    })
+                    setTimeout(() => window.location.reload(0), 1000);
+                  } else {
+                    this.setState({
+                      toDisplay:
+                        <StatusModal
+                          title="Class Creation Failed"
+                          text={err.msg}
+                          onClose={clFunc}
+                        />
+                    })
+                  }
+                })
               }}>
                 OK
               </button>
