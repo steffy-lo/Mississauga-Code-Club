ec4a8d8391f0657bf4e159868ed4c78dd6d0f32f
Nathan Fischer <CannedCans@users.noreply.github.com>
2020-03-24 14:17:04 -0400

    Removed unused d2 things

--- main-project/app/dbworker.py
+++ main-project/app/dbworker.py
@@ -13,12 +13,6 @@ MONGOURI = "mongodb://" + DBUSER + ":" + DBPASSWORD + "@ds117535.mlab.com:17535/
 mclient = MongoClient(MONGOURI)
 database = 'heroku_9tn7s7md' # This is a database within a MongoDB instance
 
-# IF DEPLOYING TO DELIVERABLE 2, USE THE FOLLOWING LINES TO SWAP THE DB
-# MONGOURI = "mongodb://" + DBUSER + ":" + DBPASSWORD + "@ds249035.mlab.com:49035/heroku_nf9149n7?retryWrites=false"
-
-# mclient = MongoClient(MONGOURI)
-# database = 'heroku_nf9149n7' # This is a database within a MongoDB instance
-
 
 def getUsers(filt={}, projection={}):
     """
