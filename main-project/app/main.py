719db71de019f50a7e7a38ee5ad7eaa607e1db34
dsoso <dragan.soso@gmail.com>
2020-03-24 23:15:57 -0400

    Set debug to false

--- main-project/app/main.py
+++ main-project/app/main.py
@@ -1055,4 +1055,4 @@ def index(path='/'):
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
