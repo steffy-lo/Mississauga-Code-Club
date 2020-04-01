48bf50755081ce6a34e3f9f04b1e29dd8045c3bb
dsoso <dragan.soso@gmail.com>
2020-04-01 03:09:59 -0400

    Made email param optional in schema

--- main-project/app/schemaprovider.py
+++ main-project/app/schemaprovider.py
@@ -19,8 +19,7 @@ class SchemaFactory:
             "paid": {"type": "number"},
             "startRange": {"type": "string"},
             "endRange": {"type": "string"}
-        },
-        "required": ["email"]
+        }
     }
 
     report_update = {
