cde25d924cbead3f5597896bdb76c8a773e70e2c
Nathan Fischer <CannedCans@users.noreply.github.com>
2020-04-08 21:25:13 -0400

    Changed to just a ?

--- main-project/app/client/src/Components/Student/StudentDash.js
+++ main-project/app/client/src/Components/Student/StudentDash.js
@@ -83,7 +83,7 @@ class StudentDash extends React.Component {
                             </dl>
                         ))}
             </div>
-		<button className="adminStyle" onClick={() => {this.setState({modalWindow: <StatusModal title="Help" text="This page shows all your or your child's classes. This allows you to see their report cards for completed classes." onClose={() => this.setState({modalWindow: ""})}/>}) }}>Need Help?</button>
+		<button className="adminStyle" onClick={() => {this.setState({modalWindow: <StatusModal title="Help" text="This page shows all your or your child's classes. This allows you to see their report cards for completed classes." onClose={() => this.setState({modalWindow: ""})}/>}) }}>?</button>
                 </div>
             </React.Fragment>
         );
